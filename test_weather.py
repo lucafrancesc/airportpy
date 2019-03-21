a3867dda23c0f7584dcc4bc353e4e12f6320b523
Luca <eto.lucafrancesco@gmail.com>
2019-03-21 16:58:04 +0000

    gitignore updated

--- test_weather.py
+++ test_weather.py
@@ -7,7 +7,6 @@ class TestWeather(unittest.TestCase):
     def setUp(self):
         self.weather = Weather()
 
-
     @patch('weather.Weather.is_safe', return_value = True)
     def test_is_safe_true(self, is_safe):
         self.assertTrue(self.weather.is_safe())
@@ -15,8 +14,6 @@ class TestWeather(unittest.TestCase):
     @patch('weather.Weather.is_safe', return_value = False)
     def test_is_safe_false(self, is_safe):
         self.assertFalse(self.weather.is_safe())
-
-
 
 if __name__ == "__main__":
     unittest.main()
