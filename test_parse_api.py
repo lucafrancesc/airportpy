70ad2d4658ebffafbed34c50ea920209bf57d9a0
Luca <eto.lucafrancesco@gmail.com>
2019-03-21 15:17:34 +0000

    Mocking api status

--- test_parse_api.py
+++ test_parse_api.py
@@ -6,10 +6,15 @@ import unittest
 class TestApi(unittest.TestCase):
     def setUp(self):
         self.api = Api()
+        self.response = self.api.api_response()
+        self.parsed_response = self.api.parsed_response()
 
-    def test_request_response(self):
-        response = self.api.api_response()
-        self.assertEqual(response.status_code, 200)
+    @patch('parse_api.requests.get')
+
+    def test_response(self, mock_get):
+        mock_get.return_value.status_code = 200
+        self.assertEqual(self.response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
