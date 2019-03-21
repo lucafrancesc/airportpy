8f0fc1d44d06ea019f5651622d1f077ece212d08
Luca <eto.lucafrancesco@gmail.com>
2019-03-21 16:21:48 +0000

    Passing an argument to parsed_response

--- parse_api.py
+++ parse_api.py
@@ -6,11 +6,17 @@ class Api:
     def __init__(self, city = 'london'):
         self.api_url = "http://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s"%(api_token(), city)
 
+
     def api_response(self):
         return requests.get(self.api_url)
 
-    def parsed_response(self):
-        if self.api_response().ok:
-            return json.loads(self.api_response().content.decode('utf-8'))
+    def parsed_response(self, response = None):
+        if response == None:
+            response = self.api_response()
+        if response.ok:
+            return json.loads(response.content.decode('utf-8'))
         else:
             return None
+
+x=Api('gatwick')
+print(x.parsed_response()['weather'][0]['main'])
